e393d0c69072b6aaec1be6275ede25ffdfcbc482
arjun289 <arjun289singh@gmail.com>
2019-04-29 14:10:10 +0800

    Add Code for IoT Data
    Made changes to tank file to add post
    call for adding sensor data

--- blockchain-tank.go
+++ blockchain-tank.go
@@ -13,25 +13,33 @@ FLAGS are:
 package main
 
 import (
-    "io"
-    // "os"
-    // "fmt"
-    "log"
-    "flag"
-    "time"
-    // "math"
-    "strconv"
-    // "runtime"
-    "net/http"
-    // "math/rand"
-    // "io/ioutil"
-    // "encoding/hex"
-    "encoding/gob"
-    // "encoding/json"
-    // "crypto/sha256"
+	// "fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"runtime"
 
-    "github.com/gorilla/mux"
-    // "github.com/davecgh/go-spew/spew"
+	// "os"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	// "math"
+	"strconv"
+	// "runtime"
+	"net/http"
+	// "math/rand"
+	// "io/ioutil"
+	// "encoding/hex"
+	"encoding/gob"
+	"encoding/json"
+
+	// "encoding/json"
+	// "crypto/sha256"
+
+	"github.com/gorilla/mux"
+	// "github.com/davecgh/go-spew/spew"
 )
 
 ///// GLOBAL FLAGS & VARIABLES
@@ -39,14 +47,15 @@ import (
 var StartTime time.Time
 
 var listenPort, totalLocs *int // listen port & total locations in the supply chain
-var dataDir *string // pathname of data directory to save IoT Data
+var dataDir *string            // pathname of data directory to save IoT Data
 
 type IoTDataPoint struct {
-    Temperature string `json:"Temperature"`
-    Humidity string `json:"Humidity"`
-    Sound string `json:"Sound"`
-    Gas string `json:"Gas"`
-    PIR string `json:"PIR"`
+	Temperature string `json:"Temperature"`
+	Humidity    string `json:"Humidity"`
+	Sound       string `json:"Sound"`
+	Gas         string `json:"Gas"`
+	PIR         string `json:"PIR"`
+	SerialNo    int
 }
 
 var IoTDataArray []IoTDataPoint // To be saved as gob file
@@ -54,11 +63,11 @@ var IoTDataArray []IoTDataPoint // To be saved as gob file
 /////
 
 type Block struct { // An element of Blockchain
-    Index int
-    Timestamp string
-    IoTDataPointEntry []IoTDataPoint
-    PrevHash string
-    ThisHash string
+	Index             int
+	Timestamp         string
+	IoTDataPointEntry []IoTDataPoint
+	PrevHash          string
+	ThisHash          string
 }
 
 var Blockchain []Block
@@ -67,75 +76,143 @@ var Blockchain []Block
 
 func init() {
 
-    gob.Register(IoTDataPoint{}) ; gob.Register(Block{}) ;
-    gob.Register(map[string]interface{}{})
+	gob.Register(IoTDataPoint{})
+	gob.Register(Block{})
+	gob.Register(map[string]interface{}{})
 
-    log.SetFlags(log.Lshortfile)
+	log.SetFlags(log.Lshortfile)
 
-    log.Printf("Welcome to Sumanta's IoT Dashboard Server!")
-    listenPort = flag.Int("port", 8085, "mux server listen port")
-    dataDir = flag.String("dataDir", "data", "pathname of data directory to save IoT Data")
-    flag.Parse()
+	log.Printf("Welcome to Sumanta's IoT Dashboard Server!")
+	listenPort = flag.Int("port", 8085, "mux server listen port")
+	dataDir = flag.String("dataDir", "data", "pathname of data directory to save IoT Data")
+	flag.Parse()
 
-    // LoadProductData() // load from existing files, if any
+	LoadIoTData() // load from existing files, if any
 
-    StartTime = time.Now()
-    StartTime = StartTime.AddDate(0, -6, 10) // random negative offset
+	StartTime = time.Now()
+	StartTime = StartTime.AddDate(0, -6, 10) // random negative offset
 }
 
 func main() {
-    log.Fatal(launchMUXServer())
+	log.Fatal(launchMUXServer())
 }
 
 func launchMUXServer() error { // launch MUX server
-    mux := makeMUXRouter()
-    log.Println("HTTP Server Listening on port:", *listenPort) // listenPort is a global flag
-    s := &http.Server{
-        Addr:           ":" + strconv.Itoa(*listenPort),
-        Handler:        mux,
-        ReadTimeout:    10 * time.Second,
-        WriteTimeout:   10 * time.Second,
-        MaxHeaderBytes: 1 << 20,
-    }
+	mux := makeMUXRouter()
+	log.Println("HTTP Server Listening on port:", *listenPort) // listenPort is a global flag
+	s := &http.Server{
+		Addr:           ":" + strconv.Itoa(*listenPort),
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
 
-    if err := s.ListenAndServe(); err != nil {
-        return err
-    }
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
-
 func makeMUXRouter() http.Handler { // create handlers
-    muxRouter := mux.NewRouter()
-    muxRouter.HandleFunc("/", handleHome).Methods("GET")
-    muxRouter.HandleFunc("/post/{Temperature}/{Humidity}/{Sound}/{Gas}/{PIR}", handlePost).Methods("GET")
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleHome).Methods("GET")
+	muxRouter.HandleFunc("/post/{Temperature}/{Humidity}/{Sound}/{Gas}/{PIR}", handlePost).Methods("GET")
 
-    return muxRouter
+	return muxRouter
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-    log.Println("handleHome() API called")
-    io.WriteString(w, "You have entered the restricted zone. Trespassing is strictly prohibited. Defaulters will be reported.")
+	log.Println("handleHome() API called")
+	io.WriteString(w, "You have entered the restricted zone. Trespassing is strictly prohibited. Defaulters will be reported.")
 }
 
 func handlePost(w http.ResponseWriter, r *http.Request) {
- 
-    params := mux.Vars(r)
- 
-    newProduct := Product{}
-    newProduct.SerialNo = len(ProductData) + 1
-    newProduct.Location = 1
-    newProduct.Name = params["Name"]
-    codeNo, err1 := strconv.Atoi(params["CodeNo"]) ; newProduct.CodeNo = codeNo
-    cost, err2 := strconv.Atoi(params["Cost"]) ; newProduct.Cost = cost
 
-    if err1 == nil && err2 == nil {
-        fmt.Println("Adding to ProductData:", newProduct)
-        ProductData = append(ProductData, newProduct)
-        gobCheck(writePdtGob(ProductData, len(ProductData)))
-        respondWithJSON(w, r, http.StatusCreated, newProduct)
-    }    
+	params := mux.Vars(r)
+
+	newPoint := IoTDataPoint{}
+	newPoint.SerialNo = len(IoTDataArray) + 1
+	newPoint.Temperature = params["Temperature"]
+	newPoint.Humidity = params["Humidity"]
+	newPoint.Sound = params["Sound"]
+	newPoint.Gas = params["Gas"]
+	newPoint.PIR = params["PIR"]
+
+	fmt.Println("Adding to SensorData:", newPoint)
+	IoTDataArray = append(IoTDataArray, newPoint)
+	gobCheck(writeIoTGob(IoTDataArray, len(IoTDataArray)))
+	respondWithJSON(w, r, http.StatusCreated, newPoint)
 }
 
+func gobCheck(e error) { // Inspired from http://www.robotamer.com/code/go/gotamer/gob.html
+	if e != nil {
+		_, file, line, _ := runtime.Caller(1)
+		log.Println(line, "\t", file, "\n", e)
+		os.Exit(1)
+	}
+}
 
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+
+	response, err := json.MarshalIndent(payload, "", "  ")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("HTTP 500: Internal Server Error"))
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func writeIoTGob(object interface{}, fileNoCount int) error {
+	filePath := *dataDir + "/IoT-data-" + strconv.Itoa(fileNoCount) + ".gob"
+	file, err := os.Create(filePath)
+	if err == nil {
+		encoder := gob.NewEncoder(file)
+		encoder.Encode(object)
+	}
+	file.Close()
+	return err
+}
+
+func LoadIoTData() { // load from existing files, if any
+	if _, err := os.Stat(*dataDir); os.IsNotExist(err) { // if *dataDir does not exist
+		log.Println("`", *dataDir, "` does not exist. Creating directory.")
+		os.Mkdir(*dataDir, 0755) // https://stackoverflow.com/questions/14249467/os-mkdir-and-os-mkdirall-permission-value
+	}
+
+	files, err := ioutil.ReadDir(*dataDir) // dataDir from flag
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mostRecectFileNo := 0
+
+	for _, file := range files {
+		fileNo, _ := strconv.Atoi(file.Name()[len("IoT-data")+1 : len(file.Name())-4])
+		if fileNo > mostRecectFileNo {
+			mostRecectFileNo = fileNo
+		}
+	}
+
+	if mostRecectFileNo == 0 {
+		log.Println("No existing IoTData")
+	} else {
+		mostRecentFile := *dataDir + "/IoT-data-" + strconv.Itoa(mostRecectFileNo) + ".gob"
+		log.Println("Loading existing IoTData from", mostRecentFile)
+		gobCheck(readGob(&IoTDataArray, mostRecentFile))
+	}
+}
+
+func readGob(object interface{}, filePath string) error {
+	file, err := os.Open(filePath)
+	if err == nil {
+		decoder := gob.NewDecoder(file)
+		err = decoder.Decode(object)
+	}
+	file.Close()
+	return err
+}
